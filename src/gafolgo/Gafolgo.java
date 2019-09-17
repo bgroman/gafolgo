d4903c8c3ec06b8b4cf02d536f6d128e0955c7d5
Benjamin Groman <benp.groman@gmail.com>
2019-09-16 22:27:40 -0400

    Created the main class.

---
+++ src/gafolgo/Gafolgo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gafolgo;
+
+/**
+ * @author BenGr
+ *
+ */
+public class Gafolgo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
