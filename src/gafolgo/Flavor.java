cbd3510cab98ea640425cec9f3ce06fb8de921ed
Benjamin Groman <benp.groman@gmail.com>
2019-09-16 23:31:44 -0400

    Added a Flavor enumeration. May change name later.

---
+++ src/gafolgo/Flavor.java
@@ -0,0 +1,12 @@
+package gafolgo;
+/**
+ * @author Benjamin Groman
+ * This enumeration represents the possible machines that can be on the floor.
+ *
+ */
+public enum Flavor {
+	Green,
+	Red,
+	Blue,
+	Yellow
+}
