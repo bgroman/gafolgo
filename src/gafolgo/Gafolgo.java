3587b4975d9d94e490b90d956b9ce88c394bad9c
Benjamin Groman <benp.groman@gmail.com>
2019-09-28 17:24:01 -0400

    Now displays a GUI, although it only displays one floor. Threads temporarily disabled.

--- src/gafolgo/Gafolgo.java
+++ src/gafolgo/Gafolgo.java
@@ -5,12 +5,14 @@ package gafolgo;
 
 import java.io.IOException;
 
+import javax.swing.JFrame;
+
 /**
  * @author Benjamin Groman
  *
  */
 public class Gafolgo {
-
+	private static JFrame myFrame;
 	/**
 	 * @param args not used.
 	 */
@@ -22,8 +24,9 @@ public class Gafolgo {
 			fms[i] = new FloorManager(startingFloor);
 			System.out.println(fms[i].getBestMetric());
 		}
+		initializeGUI(fms);
 		for (FloorManager fm : fms) {
-			fm.start();
+			//fm.start();
 		}
 		//wait for user input to terminate
 		try {
@@ -35,10 +38,20 @@ public class Gafolgo {
 		}
 		for (FloorManager fm : fms) {
 			fm.interrupt();
+			myFrame.dispose();
 		}
 		for (FloorManager fm : fms) {
 			System.out.println(fm.getLastMetric() + " < " + fm.getBestMetric());
 		}
+	}
+	private static void initializeGUI(FloorManager[] fms) {
+		myFrame = new JFrame("Gafolgo");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		for (FloorManager fm : fms) {
+			myFrame.getContentPane().add(new FloorPanel(fm.getBestLayout(), fm.getBestMetric()));
+		}
+		myFrame.pack();
+		myFrame.setVisible(true);
 	}
 
 }
